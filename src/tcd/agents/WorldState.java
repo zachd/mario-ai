700cd2d12ba837d8789dd31f3d4ecbab6eb0d274
Iain Meeke <iain.meeke@enernoc.com>
2017-01-17 16:30:29 +0000

    added world state class

---
+++ src/tcd/agents/WorldState.java
@@ -0,0 +1,17 @@
+package tcd.agents;
+
+/**
+ * This object will hold the state of the mario world.
+ */
+public class WorldState {
+    
+
+    /**
+     * worldstate constructor
+     */
+    public WorldState(){
+
+    }
+}
+
+
